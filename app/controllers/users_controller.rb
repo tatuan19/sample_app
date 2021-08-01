d2cce297b1ecd2a2996b6b1153baae74dbbfe2cb
tatuan19 <trananhtuan12a10@gmail.com>
2021-08-01 13:13:49 +0700

    Chapter 8: Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "welcome_to_the_sample_app!"
       redirect_to @user
     else
-      flash[:danger] = t "signup_fail"
+      flash[:danger] = t "error.signup_fail"
       render :new
     end
   end
@@ -18,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.find_by id: params[:id]
     return if @user
 
-    flash[:danger] = t "not_found"
+    flash[:danger] = t "error.not_found"
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
+
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users
   end
 end
